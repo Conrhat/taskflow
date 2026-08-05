f21176ef884822624e75fb4be5d14b6f6e5766e9
Conrhat <patrickvilla11037@gmail.com>
2026-08-04 22:16:58 -0500

    Creacion de Layouts

--- laravel/resources/views/about.blade.php
+++ laravel/resources/views/about.blade.php
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>Hola mundo xd</p>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+    <div class="bg-gray-100 p-4 rounded-lg shadow-md">
+        <h1 class="font-serif text-2xl">Sobre Nosotros</h1>
+        <h2 class="font-serif text-lg text-gray-700">Conoce más sobre nuestra empresa y nuestro equipo.</h2>
+    </div>
+
+    <div class="flex flex-col items-center text-wrap p-5">
+        <p class="font-serif text-justify">Somos una empresa dedicada a brindar soluciones innovadoras en el ámbito tecnológico. Nuestro equipo está compuesto por profesionales apasionados por la tecnología y comprometidos con la excelencia.</p>
+        <p class="font-serif text-justify">Nuestra misión es ofrecer productos y servicios de alta calidad que satisfagan las necesidades de nuestros clientes y contribuyan al desarrollo del sector tecnológico.</p>
+    </div>
+
+    <div class="bg-gray-100 p-4 rounded-lg shadow-md flex flex-col items-center">
+        <p>Equipo | Innovación | Tecnología | Compromiso | Excelencia</p>
+    </div>
+@endsection
